3808edfa3481cfe35c3c4aaeb3571ab8e2c172b5
micholle <cortezanomicholle@gmail.com>
2023-05-09 04:35:09 +0800

    added calendar

---
+++ views/modules/calendar.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+    <head> 
+        <title>Calendar</title>
+        <script type="text/javascript" src="../assets/js/jquery-3.6.4.min.js"></script>
+        <script type="text/javascript" src="../assets/plugins/bootstrap-4.0.0/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="../js/script.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="../assets/plugins/bootstrap-4.0.0/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="../assets/css/style.css">
+    </head>
+    <body>
+        <div>
+            <iframe id="open-web-calendar" 
+                style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
+                src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fen.christian%2523holiday%2540group.v.calendar.google.com%2Fpublic%2Fbasic.ics&amp;url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fen.hinduism%2523holiday%2540group.v.calendar.google.com%2Fpublic%2Fbasic.ics&amp;url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fen.judaism%2523holiday%2540group.v.calendar.google.com%2Fpublic%2Fbasic.ics&amp;url=https%3A%2F%2Fcalendar.google.com%2Fcalendar%2Fical%2Fen.islamic%2523holiday%2540group.v.calendar.google.com%2Fpublic%2Fbasic.ics&amp;title=Religion%20Explorer"
+                sandbox="allow-scripts allow-same-origin allow-top-navigation"
+                allowTransparency="true" scrolling="no"frameborder="0" height="600px" width="100%">
+            </iframe>
+            </div>
+    </body>
+</html>
