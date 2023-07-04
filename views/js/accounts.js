4b768baf98d213d7f930eef1455e95b2e337251a
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-04 23:49:38 +0800

    Update accounts.js

--- views/js/accounts.js
+++ views/js/accounts.js
@@ -1,25 +1,4 @@
 $(function() {
-   $("#btn-alert").click(function (e) {
-      e.preventDefault();
-      var email = $("#email").val();
-      var acctype = "regular";
-      var religion = $("#religion").val();
-      var username = $("#username").val();
-      var password = $("#password").val();
-  
-      // Create an object to hold the data
-      var formData = {
-        email: email,
-        acctype: acctype,
-        religion: religion,
-        username: username,
-        password: password
-      };
-  
-      // Display the data in an alert
-      alert(JSON.stringify(formData));
-    });
- 
    // SAVE account
    $("form").submit(function(e) {
      e.preventDefault();
