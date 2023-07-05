b6ee95aa1f98b80c6e35bae71fa8223713d43c0e
(andrea.ymanuelle) <(andrea.ymanuelle@gmail.com)>
2023-07-06 05:57:55 +0800

    library and community ui updates

---
+++ views/modules/communitySubmissions.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Religion Explorer: Community Creations</title>
+        <link rel="icon" type="image/x-icon" href="../assets/img/applogo.png">
+        <script type="text/javascript" src="../assets/js/jquery-3.6.4.min.js"></script>
+        <script type="text/javascript" src="../assets/plugins/bootstrap-4.0.0/js/bootstrap.bundle.min.js"></script>
+
+        <script type="text/javascript" src="../js/communitySubmissions.js"></script>
+        <script type="text/javascript" src="../js/script.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="../assets/plugins/bootstrap-4.0.0/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="../assets/css/styles.css">
+    </head>
+    <body>
+        <div id="communitySubmissionsSidebar"></div>
+
+        <div class="pageContainer">
+            <div class="container mw-100 mh-100">
+                <div class="row d-flex justify-content-center align-items-center pageHeader">
+                    <div class="col-4 d-flex justify-content-start align-items-center">
+                        <h1>Community Creations</h1>
+                    </div>
+                    <div class="col-8 d-flex justify-content-start align-items-center">
+                        <input type="search" id="communitySearch" name="communitySearch" placeholder="Search the Community">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row pageContent">
+                <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+                    <div class="libraryMediaContainer d-flex flex-column">
+                        <div class="row d-flex justify-content-center align-items-center">
+                            <div class="col-2">
+                                <img src="../assets/img/lamb.png">
+                            </div>
+                            <div class="col-10 libraryMediaHeader">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <h1>Placeholder Title</h1>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-12 d-flex flex-row">
+                                        <p>Placeholder User</p><p>•</p><p>Placeholder Date</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 d-flex justify-content-center align-items-center">
+                                <img src="../assets/img/placeholder.png">
+                            </div>
+                        </div>
+
+                        <div class="row d-flex justify-content-start align-items-center flex-row libraryMediaInteractions">
+                            <div class="col-11 d-flex justify-content-start align-items-center mediaInteractionsLeft">
+                                <img src="../assets/img/download.png">
+                                <img src="../assets/img/alert.png">
+                                <img src="../assets/img/broken-link.png">
+                            </div>
+                            <div class="col-1 d-flex justify-content-end align-items-center mediaInteractionsRight">
+                                <img src="../assets/img/bookmark-white.png">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at leo sed justo dignissim viverra. Nulla facilisi. Aliquam efficitur at eros eget interdum. Nulla eget ipsum orci. Praesent varius non lacus ac cursus. Curabitur metus nibh, pellentesque maximus mi non, hendrerit fringilla purus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </body>
+</html>
---
+++ views/modules/libraryMedia.php
@@ -0,0 +1,97 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <title>Religion Explorer: Library of Resources</title>
+        <link rel="icon" type="image/x-icon" href="../assets/img/applogo.png">
+        <script type="text/javascript" src="../assets/js/jquery-3.6.4.min.js"></script>
+        <script type="text/javascript" src="../assets/plugins/bootstrap-4.0.0/js/bootstrap.bundle.min.js"></script>
+
+        <script type="text/javascript" src="../js/libraryMedia.js"></script>
+        <script type="text/javascript" src="../js/script.js"></script>
+
+        <link type="text/css" rel="stylesheet" href="../assets/plugins/bootstrap-4.0.0/css/bootstrap.min.css">
+        <link type="text/css" rel="stylesheet" href="../assets/css/styles.css">
+    </head>
+    <body>
+        <div id="libraryMediaSidebar"></div>
+
+        <div class="pageContainer">
+            <div class="container mw-100 mh-100">
+                <div class="row d-flex justify-content-center align-items-center pageHeader">
+                    <div class="col-4 d-flex justify-content-start align-items-center">
+                        <h1>Library of Resources</h1>
+                    </div>
+                    <div class="col-8 d-flex justify-content-start align-items-center">
+                        <input type="search" id="communitySearch" name="communitySearch" placeholder="Search the Community">
+                    </div>
+                </div>
+            </div>
+
+            <div class="row pageContent">
+                <div class="col-12 d-flex justify-content-center align-items-center flex-column">
+                    <div class="libraryMediaContainer d-flex flex-column">
+                        <div class="row">
+                            <div class="col-12 libraryMediaHeader">
+                                <h1>Placeholder Title</h1>
+                                <p>Placeholder Date</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 d-flex justify-content-center align-items-center">
+                                <img src="../assets/img/placeholder.png">
+                            </div>
+                        </div>
+
+                        <div class="row d-flex justify-content-start align-items-center flex-row libraryMediaInteractions">
+                            <div class="col-11 d-flex justify-content-start align-items-center mediaInteractionsLeft">
+                                <img src="../assets/img/download.png">
+                                <img src="../assets/img/broken-link.png">
+                            </div>
+                            <div class="col-1 d-flex justify-content-end align-items-center mediaInteractionsRight">
+                                <img src="../assets/img/bookmark-white.png">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at leo sed justo dignissim viverra. Nulla facilisi. Aliquam efficitur at eros eget interdum. Nulla eget ipsum orci. Praesent varius non lacus ac cursus. Curabitur metus nibh, pellentesque maximus mi non, hendrerit fringilla purus.</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="libraryMediaContainer d-flex flex-column">
+                        <div class="row">
+                            <div class="col-12 libraryMediaHeader">
+                                <h1>Placeholder Title</h1>
+                                <p>Placeholder Date</p>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12 d-flex justify-content-center align-items-center">
+                                <img src="../assets/img/placeholder.png">
+                            </div>
+                        </div>
+
+                        <div class="row d-flex justify-content-start align-items-center flex-row libraryMediaInteractions">
+                            <div class="col-11 d-flex justify-content-start align-items-center mediaInteractionsLeft">
+                                <img src="../assets/img/download.png">
+                                <img src="../assets/img/broken-link.png">
+                            </div>
+                            <div class="col-1 d-flex justify-content-end align-items-center mediaInteractionsRight">
+                                <img src="../assets/img/bookmark-white.png">
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque at leo sed justo dignissim viverra. Nulla facilisi. Aliquam efficitur at eros eget interdum. Nulla eget ipsum orci. Praesent varius non lacus ac cursus. Curabitur metus nibh, pellentesque maximus mi non, hendrerit fringilla purus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
