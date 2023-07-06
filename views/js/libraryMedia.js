7b503475e671cf1f818df7d3db90513e7908c597
andreaymanuelle <79954327+andreaymanuelle@users.noreply.github.com>
2023-07-06 20:45:40 +0800

    Delete libraryMedia.js
    void.

--- views/js/libraryMedia.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    $.ajax({
-        url: "../../ajax/showSidebar.ajax.php",
-        method: "POST",
-        success:function(data){
-            $("#libraryMediaSidebar").html(data);
-        }
-    });
-});
