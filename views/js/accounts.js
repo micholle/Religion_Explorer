8e98f278444e1729f2035ac596e174d4c61412e6
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-06 19:19:18 +0800

    Update accounts.js

--- views/js/accounts.js
+++ views/js/accounts.js
@@ -8,6 +8,8 @@ $(function() {
      var religion = $("#religion").val();
      var username = $("#username").val();
      var password = $("#password").val();
+     var verified = '0';
+     var verificationCode = Math.floor(100000 + Math.random() * 900000);
  
      var account = new FormData();
      account.append("email", email);
@@ -15,6 +17,8 @@ $(function() {
      account.append("religion", religion);
      account.append("username", username);
      account.append("password", password);
+     account.append("verified", verified);
+     account.append("verificationCode", verificationCode);
  
      $.ajax({
        url: "../../ajax/accountSave.ajax.php",
@@ -25,6 +29,7 @@ $(function() {
        processData: false,
        dataType: "text",
        success: function(answer) {
+        window.location.href = '../../models/accounts.model.php';
        },
        error: function() {
          alert("Oops. Something went wrong!");
@@ -34,5 +39,32 @@ $(function() {
        }
      });
    });
+
+   $("#verify").onclick(function() {
+    var email = $("#email").val();
+    var verificationCode = $("#verificationCode").val();
+
+    var verify = new FormData();
+    account.append("email", email);
+    account.append("verificationCode", verificationCode);
+
+    $.ajax({
+      url: "../../ajax/verifyCode.ajax.php",
+      method: "POST",
+      data: {verify},
+      dataType: "text",
+      success: function(answer) {
+        if (answer === "ok") {
+          alert("Account verified Successfully!");
+        } else {
+          // Verification code is incorrect
+          alert("Verification code is incorrect. Please try again.");
+        }
+      },
+      error: function() {
+        alert("Oops. Something went wrong!");
+      }
+    });
+  });
  });
  
