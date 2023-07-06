88610d224b23921a83fb5dc9d1b84660f07aec60
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-07 02:24:01 +0800

    Update forgotPassword.js

--- views/js/forgotPassword.js
+++ views/js/forgotPassword.js
@@ -12,7 +12,37 @@ $(function() {
 
     const forgotSubmitButton = document.getElementById("forgotPasswordSubmit");
 
-    forgotSubmitButton.addEventListener("click", () => {
-        forgotContainer.innerHTML = forgotNewContent;
+    forgotSubmitButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        const email = $("#email").val().trim(); // Get the entered email
+    
+        if (email === "") {
+          alert("Please enter your email address.");
+          return;
+        }
+
+        // Make an AJAX request to check if email exists and send email
+        $.ajax({
+            url: "../../ajax/forgotPassword.ajax.php",
+            method: "POST",
+            data: { email: email },
+            dataType: "text",
+            success: function(response) {
+            // Handle the response from the server
+            if (response === "ok") {
+                // Email exists and email sent successfully
+                forgotContainer.innerHTML = forgotNewContent;
+            } else if (response === "notfound") {
+                // Email not found in the database
+                alert("Email not found. Please try again.");
+            } else {
+                // Error occurred while sending email
+                alert("Oops. Something went wrong!");
+            }
+            },
+            error: function() {
+            alert("Oops. Something went wrong!");
+            }
+        });
     });
 });
