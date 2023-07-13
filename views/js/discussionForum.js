7c20af84119308d74499e62d86619f30c8a47b7f
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-13 20:39:53 +0800

    Update discussionForum.js

--- views/js/discussionForum.js
+++ views/js/discussionForum.js
@@ -1,4 +1,20 @@
 $(function() {
+    $(document).ready(function() {
+        $.ajax({
+            url: "../../ajax/discussionTopics.ajax.php",
+            method: "GET",
+            success: function(data) {
+                console.log(data);
+                $("#topicsContainer").html(data);
+            },
+            error: function(xhr, status, error) {
+                console.log(xhr.responseText);
+                console.log(status);
+                console.log(error);
+            }
+        });
+      });
+
     $.ajax({
         url: "../../ajax/showSidebar.ajax.php",
         method: "POST",
@@ -7,7 +23,7 @@ $(function() {
         }
     });
 
-    $(".forumPostContainer").click(function(){
+    $(document).on("click", ".forumContent", function(){
         window.location.href = "discussionForumPost.php";
     });
 });
