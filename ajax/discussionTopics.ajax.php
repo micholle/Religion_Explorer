bcf0d34745fa17e69a4ba5ea0edd1e3630ed1ea5
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-14 05:25:45 +0800

    Update discussionTopics.ajax.php

--- ajax/discussionTopics.ajax.php
+++ ajax/discussionTopics.ajax.php
@@ -1,15 +1,20 @@
 <?php
 require_once "../controllers/discussion.controller.php";
 
+$sort = isset($_GET['sort']) ? $_GET['sort'] : ''; // Get the sort criteria from the request
+
+// Create an instance of the ControllerDiscussion class
 $controller = new ControllerDiscussion();
-$topics = $controller->ctrGetAllTopics();
+
+// Fetch topics using the controller with the sort criteria
+$topics = $controller->ctrGetAllTopics($sort);
 
 $html = '';
 foreach ($topics as $topic) {
   $html .= '<div class="forumPostContainer">';
   $html .= '<div class="d-flex justify-content-start align-items-center flex-column forumInteractions">';
   $html .= '<img src="../assets/img/discussionForum/upvote.png">';
-  $html .= '<p>0</p>';
+  $html .= '<p class="upvotes">' . $topic['upvotes'] . '</p>';
   $html .= '<img src="../assets/img/discussionForum/downvote.png">';
   $html .= '<img src="../assets/img/discussionForum/comments.png" class="commentIcon">';
   $html .= '<p>0</p>';
@@ -24,8 +29,10 @@ foreach ($topics as $topic) {
   $html .= '</div>';
   $html .= '</div>';
   $html .= '<p>' . $topic['topicContent'] . '</p>';
+  $html .= '<input type="hidden" class="topicId" id="topicId" value="' . $topic['topicId'] . '">';
   $html .= '</div>';
   $html .= '</div>';
+
 }
 
 echo $html;
