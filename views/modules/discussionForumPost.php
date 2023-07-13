eadc3e358edd43aebc5fab797fab20e8a7f2be54
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-14 05:19:48 +0800

    Update discussionForumPost.php

--- views/modules/discussionForumPost.php
+++ views/modules/discussionForumPost.php
@@ -1,3 +1,9 @@
+<?php
+require_once "../../models/discussionForumPost.model.php";
+?>
+
+
+
 <!doctype html>
 <html lang="en">
     <head>
@@ -18,9 +24,12 @@
         <div class="pageContainer">
             <div class="container mw-100 mh-100">
                 <div class="row d-flex justify-content-center align-items-center pageHeader">
-                    <div class="col-12 d-flex justify-content-start align-items-center">
+                    <div class="col-4 d-flex justify-content-start align-items-center">
                         <h1>Discussion Forum</h1>
                     </div>  
+                    <div class="col-8 d-flex justify-content-start align-items-center">
+                        <input type="search" id="communitySearch" name="communitySearch" placeholder="Search the Community">
+                    </div>
                 </div>
             </div>
 
@@ -29,21 +38,21 @@
                     <div class="forumPostViewBox">
                         <div class="forumPostViewMain">
                             <div class="row">
-                                <div class="col-12 d-flex flex-column">
-                                    <h1>[Placeholder Title]</h1>
+                            <div class="col-12 d-flex flex-column">
+                                    <h1><?php echo $topicTitle; ?></h1>
                                     <div class="row">
                                         <div class="col-12 d-flex flex-row">
-                                            <h2>by [Placeholder]</h2><h2>•</h2><h2>[Placeholder Time]</h2>
+                                            <h2>by <?php echo $username; ?></h2><h2>•</h2><h2><?php echo $topicDate; ?></h2>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-12">
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies eros id malesuada porta. Sed id vulputate purus. Vestibulum non luctus nisi, vel tincidunt leo. Phasellus scelerisque metus sit amet est pulvinar, efficitur molestie elit laoreet. Praesent porttitor sagittis nunc, non facilisis lorem vestibulum non. Ut eu elementum nulla, dictum laoreet lacus. Fusce tempus, augue bibendum convallis luctus, mi eros vehicula purus, at aliquam diam dolor et tellus. Nullam ac ullamcorper risus. Praesent ultrices sapien eu lacus ornare tincidunt. Sed vitae convallis nulla. Sed porta dignissim metus, non porta diam dictum vitae. Duis cursus ligula quis sapien aliquet maximus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+                                    <p><?php echo $topicContent; ?></p>
                                 </div>
                                 <div class="col-12 d-flex flex-row">
                                     <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
                                         <img src="../assets/img/discussionForum/upvote.png">
-                                        <p class="forumPostViewMainCount forumPostViewMainVote">0</p>
+                                        <p class="forumPostViewMainCount forumPostViewMainVote" id="upvotes" data-upvotes="<?php echo $upvotes; ?>"></p>
                                         <img src="../assets/img/discussionForum/downvote.png">
                                     </div>
                                     <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
@@ -57,6 +66,7 @@
                                     <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row" id="forumDeletePost">
                                         <img src="../assets/img/discussionForum/delete.png" class="commentIcon">
                                         <p class="forumPostViewMainCount forumPostViewMainDelete">Delete</p>
+                                        <input type="hidden" class="topicId" id="topicId" value="<?php echo $topicId; ?>">
                                     </div>
                                 </div>
                             </div>
@@ -72,7 +82,7 @@
                             <div class="forumCommentContainer">
                                 <div class="forumCommentContent">
                                     <form>
-                                        <textarea placeholder="What are your thoughts?"></textarea>
+                                        <textarea placeholder="What are your thoughts?" id="postContent"></textarea>
                                         <div class="row">
                                             <div class="col-12 d-flex justify-content-end align-items-center flex-row">
                                                 <button type="submit" class="roundedButton">Comment</button>
@@ -82,46 +92,8 @@
                                 </div>
                             </div>
                         </div>
-
-                        <div class="forumPostViewThread d-flex flex-column">
-                            <div class="forumPostViewComments">
-                                <div class="d-flex justify-content-start align-items-center flex-column">
-                                    <img src="../assets/img/editProfile/lion.png">
-                                </div>
-                                <div class="forumPostViewContent">
-                                    <div class="row">
-                                        <div class="col-12 d-flex flex-row">
-                                            <h2>[Placeholder User]</h2><h2>•</h2><h2>[Placeholder Time]</h2>
-                                        </div>
-                                    </div>
-                                    <div class="forumPostViewContentBox">
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies eros id malesuada porta. Sed id vulputate purus. Vestibulum non luctus nisi, vel tincidunt leo. Phasellus scelerisque metus sit amet est pulvinar, efficitur molestie elit laoreet.<p>
-                                        <div class="col-12 d-flex flex-row forumPostViewContentInt">
-                                            <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-                                                <img src="../assets/img/discussionForum/upvote.png">
-                                                <p class="forumPostViewMainCount forumPostViewMainVote">0</p>
-                                                <img src="../assets/img/discussionForum/downvote.png">
-                                            </div>
-                                            <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-                                                <img src="../assets/img/discussionForum/comments.png" class="commentIcon">
-                                                <p class="forumPostViewMainCount forumPostViewMainComment">Reply</p>
-                                            </div>
-                                            <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-                                                <img src="../assets/img/discussionForum/report.png" class="commentIcon">
-                                                <p class="forumPostViewMainCount forumPostViewMainReport">Report</p>
-                                            </div>
-                                            <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row" id="forumDeleteComment">
-                                                <img src="../assets/img/discussionForum/delete.png" class="commentIcon">
-                                                <p class="forumPostViewMainCount forumPostViewMainDelete">Delete</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>  
-
-                            <div id="forumPostViewCommentReplySubmit"></div>
-
-                            <div id="forumPostViewCommentReply">
+                        <div id="postContainer"></div>
+                        
                                 <!-- <div class="forumPostViewComments">
                                     <div class="d-flex justify-content-start align-items-center flex-column">
                                         <img src="../assets/img/editProfile/lion.png">
@@ -152,9 +124,8 @@
                                         </div>
                                     </div>
                                 </div> -->
-                            </div>
 
-                        </div>
+                        
 
                     </div>
                 </div>
@@ -173,51 +144,7 @@
                 </div>
             </div>
 
-            <div class="modal fade" id="confirmDeleteModalPost">
-                <div class="modal-dialog modal-xs modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-12 d-flex justify-content-center align-items-center flex-column">
-                                        <h5 class="modal-title w-100">Delete Post?</h5>
-                                        <p>Are you sure you want this gone?</p>
-                                        <div class="row">
-                                            <div class="col-12 d-flex flex-row">
-                                                <button type="button" class="roundedButtonVariant">Cancel</button>
-                                                <button type="button" class="roundedButton">Delete</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div class="modal fade" id="confirmDeleteModalComment">
-                <div class="modal-dialog modal-xs modal-dialog-centered">
-                    <div class="modal-content">
-                        <div class="modal-body">
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-12 d-flex justify-content-center align-items-center flex-column">
-                                        <h5 class="modal-title w-100">Delete Comment?</h5>
-                                        <p>Are you sure you want this gone?</p>
-                                        <div class="row">
-                                            <div class="col-12 d-flex flex-row">
-                                                <button type="button" class="roundedButtonVariant">Cancel</button>
-                                                <button type="button" class="roundedButton">Delete</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
     </body>
 </html>
