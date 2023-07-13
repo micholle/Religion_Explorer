0c190af6bf209c976ead100d956b2402361b1d73
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-13 21:56:17 +0800

    Update discussionForum.js

--- views/js/discussionForum.js
+++ views/js/discussionForum.js
@@ -1,5 +1,50 @@
 $(function() {
     $(document).ready(function() {
+        getTopics();
+    });
+
+    $("form").submit(function(e) {
+        e.preventDefault();
+        createDiscussion();
+    });
+
+    function createDiscussion() {
+        // Retrieve the form data
+        var topicTitle = $("#topicTitle").val();
+        var topicContent = $("#topicContent").val();
+    
+        // Create an object with the data
+        var discussion = {
+          topicTitle: topicTitle,
+          topicContent: topicContent
+        };
+    
+        // Make the AJAX request to create the topic
+        $.ajax({
+          url: "../../ajax/discussionCreate.ajax.php",
+          method: "POST",
+          data: discussion,
+          success: function(response) {
+            if (response === "success") {
+              // Topic created successfully
+              alert("Topic created successfully!");
+              $("#topicTitle").val("");
+              $("#topicContent").val("");
+              // Refresh the topics by calling the getTopics function
+              getTopics();
+            } else {
+              // Error occurred while creating the topic
+              alert("Error occurred while creating the topic.");
+            }
+          },
+          error: function() {
+            // AJAX request failed
+            alert("Error occurred while making the AJAX request.");
+          }
+        });
+    }
+
+    function getTopics() {
         $.ajax({
             url: "../../ajax/discussionTopics.ajax.php",
             method: "GET",
@@ -13,7 +58,7 @@ $(function() {
                 console.log(error);
             }
         });
-      });
+    }
 
     $.ajax({
         url: "../../ajax/showSidebar.ajax.php",
@@ -27,3 +72,4 @@ $(function() {
         window.location.href = "discussionForumPost.php";
     });
 });
+    
