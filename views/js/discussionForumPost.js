c6466c57582f6859dfea044f0584c72a130ba0a8
JcAngeles1024 <124694435+JcAngeles1024@users.noreply.github.com>
2023-07-16 03:53:55 +0800

    Update discussionForumPost.js

--- views/js/discussionForumPost.js
+++ views/js/discussionForumPost.js
@@ -1,17 +1,4 @@
 $(function() {
-    $(document).ready(function() {
-        // Retrieve the topic data from localStorage
-        var topicData = JSON.parse(localStorage.getItem("topicData"));
-      
-        // Populate the elements with the topic data
-        $("#topicTitle").text(topicData.topicTitle);
-        $("#topicDetails").text("by " + topicData.username + " • " + topicData.topicDate);
-        $("#topicContent").text(topicData.topicContent);
-      
-        // Clear the topic data from localStorage
-        localStorage.removeItem("topicData");
-      });
-    
     $.ajax({
         url: "../../ajax/showSidebar.ajax.php",
         method: "POST",
@@ -22,156 +9,82 @@ $(function() {
 
     function initializeReplyButtons() {
         const replyButtons = document.querySelectorAll('.forumPostViewMainComment');
-        const replyContainerWrapper = document.getElementById('forumPostViewCommentReplySubmit');
-        let replyContainer = null; // Variable to store the reply container reference
-    
+      
         replyButtons.forEach((replyButton) => {
-            replyButton.addEventListener('click', function() {
+            replyButton.addEventListener('click', function () {
                 const parentComment = replyButton.closest('.forumPostViewComments');
-    
-                if (parentComment === replyContainer) {
-                    replyContainer.remove(); // Remove the reply container if it belongs to the clicked comment
-                    replyContainer = null; // Reset the reference to null
+                const postId = $(this).attr('value'); // Get the post ID from the data attribute
+                const replyContainer = parentComment.querySelector('.threadReply');
+          
+                if (replyContainer) {
+                    replyContainer.remove(); // Remove the reply container if it exists
                 } else {
                     const contentHTML = `
                         <div class="threadReply" style="width: 100%;">
                             <div class="col-12 forumCommentMargin">
                                 <div class="threadReplyLine">
-                                <div class="forumCommentContainer">
-                                    <div class="forumCommentContent">
-                                    <form>
-                                        <textarea placeholder="What are your thoughts?"></textarea>
-                                        <div class="row">
-                                        <div class="col-12 d-flex justify-content-end align-items-center flex-row">
-                                            <button type="button" class="roundedButtonVariant" id="cancelReplyButton">Cancel</button>
-                                            <button type="submit" class="roundedButton">Reply</button>
+                                    <div class="forumCommentContainer">
+                                        <div class="forumCommentContent">
+                                            <form>
+                                                <textarea placeholder="What are your thoughts?"></textarea>
+                                                <div class="row">
+                                                    <div class="col-12 d-flex justify-content-end align-items-center flex-row">
+                                                        <button type="button" class="roundedButtonVariant" id="cancelReplyButton">Cancel</button>
+                                                        <button type="submit" class="roundedButton">Reply</button>
+                                                    </div>
+                                                </div>
+                                            </form>
                                         </div>
-                                        </div>
-                                    </form>
                                     </div>
-                                </div>
                                 </div>
                             </div>
                         </div>
                     `;
-    
-                    // Remove the existing reply container if it exists
-                    if (replyContainer) {
-                        replyContainer.remove();
-                    }
-    
-                    replyContainer = document.createElement('div');
-                    replyContainer.classList.add('forumPostViewComments');
-    
+          
+                    const replyContainer = document.createElement('div');
+                    replyContainer.classList.add('threadReply');
                     replyContainer.innerHTML = contentHTML;
-    
+          
                     parentComment.appendChild(replyContainer);
-    
-                    // Attach event listener to the cancel button
+          
                     const cancelReplyButton = replyContainer.querySelector('#cancelReplyButton');
-                    cancelReplyButton.addEventListener('click', function() {
-                        replyContainer.remove(); // Remove the reply container when the cancel button is clicked
-                        replyContainer = null; // Reset the reference to null
+                    cancelReplyButton.addEventListener('click', function () {
+                        replyContainer.remove();
+                    });
+          
+                    const replyForm = replyContainer.querySelector('form');
+                    replyForm.addEventListener('submit', function (e) {
+                        e.preventDefault();
+                        const replyContent = replyForm.querySelector('textarea').value;
+                        createReply(postId, replyContent); // Pass the postId and replyContent to createReply function
                     });
                 }
             });
         });
-    
-        // Close reply container if clicking outside
-        document.addEventListener('click', function(event) {
-            const isOutsideContainer = !event.target.closest('.forumPostViewComments');
-            if (isOutsideContainer && replyContainer) {
-                replyContainer.remove();
-                replyContainer = null;
+    }
+
+    // Create a new reply
+    function createReply(postId, replyContent) {
+        const postData = {
+            postId: postId,
+            replyContent: replyContent
+        };
+
+        $.ajax({
+            url: "../../ajax/discussionReply.ajax.php",
+            method: "POST",
+            data: postData,
+            success: function(response) {
+                if (response === "success") {
+                    getPosts("", $("#topicId").val());
+                } else {
+                    alert("Error occurred while creating the reply.");
+                }
+            },
+            error: function() {
+                alert("Error occurred while making the AJAX request.");
             }
         });
-    
-        // Append the reply container wrapper to the document body
-        document.body.appendChild(replyContainerWrapper);
-    }
-    
-    
-
-
-    // //NOT FINAL NOT FINAL NOT FINAL NOT FINAL//
-    // // Find all elements with the class "forumPostViewMainComment"
-    // const replyButtons = document.querySelectorAll('.forumPostViewMainComment');
-
-    // // Attach a click event listener to each reply button
-    // replyButtons.forEach((replyButton) => {
-    //     replyButton.addEventListener('click', function() {
-    //         // Create the HTML code for the content block
-    //         const contentHTML = `
-    //         <div class="forumPostViewComments">
-    //             <div class="d-flex justify-content-start align-items-center flex-column">
-    //             <img src="../assets/img/editProfile/lion.png">
-    //             </div>
-    //             <div class="forumPostViewContent">
-    //             <div class="row">
-    //                 <div class="col-12 d-flex flex-row">
-    //                 <h2>[Placeholder User]</h2><h2>•</h2><h2>[Placeholder Time]</h2>
-    //                 </div>
-    //             </div>
-    //             <div class="forumPostViewContentBox">
-    //                 <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ultricies eros id malesuada porta. Sed id vulputate purus. Vestibulum non luctus nisi, vel tincidunt leo. Phasellus scelerisque metus sit amet est pulvinar, efficitur molestie elit laoreet.</p>
-    //                 <div class="col-12 d-flex flex-row forumPostViewContentInt">
-    //                 <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-    //                     <img src="../assets/img/discussionForum/upvote.png">
-    //                     <p class="forumPostViewMainCount forumPostViewMainVote">0</p>
-    //                     <img src="../assets/img/discussionForum/downvote.png">
-    //                 </div>
-    //                 <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-    //                     <img src="../assets/img/discussionForum/comments.png" class="commentIcon">
-    //                     <p class="forumPostViewMainCount forumPostViewMainComment">Reply</p>
-    //                 </div>
-    //                 <div class="forumPostViewMainInt d-flex justify-content-center align-items-center flex-row">
-    //                     <img src="../assets/img/discussionForum/report.png" class="commentIcon">
-    //                     <p class="forumPostViewMainCount forumPostViewMainReport">Report</p>
-    //                 </div>
-    //                 </div>
-    //             </div>
-    //             </div>
-    //         </div>
-    //         `;
-
-    //         // Create a new reply container element
-    //         const replyContainer = document.createElement('div');
-    //         replyContainer.classList.add('forumPostViewComments');
-
-    //         // Set the content of the reply container
-    //         replyContainer.innerHTML = contentHTML;
-
-    //         // Append the reply container to the forumPostViewCommentReply element
-    //         const replyContainerWrapper = document.getElementById('forumPostViewCommentReply');
-    //         replyContainerWrapper.appendChild(replyContainer);
-    //     });
-    // });
-
-    // Get the HTML element with the specific ID to display the upvotes
-    var upvotesElement = document.getElementById("upvotes");
-
-    // Retrieve the upvotes value from the data attribute
-    var upvotes = parseInt(upvotesElement.getAttribute("data-upvotes"));
-
-    // Set the shortened upvotes value using the 'shortenUpvotes' function
-    upvotesElement.textContent = shortenUpvotes(upvotes);
-
-    function shortenUpvotes(upvotes) {
-    var SI_POSTFIXES = ["", "K", "M", "B", "T"];
-    var tier = Math.log10(Math.abs(upvotes)) / 3 | 0;
-
-    if (tier === 0) return upvotes;
-
-    var postfix = SI_POSTFIXES[tier];
-    var scale = Math.pow(10, tier * 3);
-    var scaledNumber = upvotes / scale;
-
-    // Remove decimal if the number is a whole number
-    if (scaledNumber % 1 === 0) {
-        scaledNumber = Math.floor(scaledNumber);
-    }
-
-    return scaledNumber.toFixed(0) + postfix;
     }
 
     $(document).ready(function() {
@@ -190,31 +103,30 @@ $(function() {
     
         // Create an object with the data
         var discussion = {
-          topicId: topicId,
-          postContent: postContent
+            topicId: topicId,
+            postContent: postContent
         };
     
         // Make the AJAX request to create the topic
         $.ajax({
-          url: "../../ajax/discussionPost.ajax.php",
-          method: "POST",
-          data: discussion,
-          success: function(response) {
-            if (response === "success") {
-              // Topic created successfully
-              alert("Post created successfully!");
-              $("#postContent").val("");
-              // Refresh the topics by calling the getTopics function
-              getPosts("", $("#topicId").val());
-            } else {
-              // Error occurred while creating the topic
-              alert("Error occurred while creating the post.");
+            url: "../../ajax/discussionPost.ajax.php",
+            method: "POST",
+            data: discussion,
+            success: function(response) {
+                if (response === "success") {
+                    // Topic created successfully
+                    $("#postContent").val("");
+                    // Refresh the topics by calling the getTopics function
+                    getPosts("", $("#topicId").val());
+                } else {
+                    // Error occurred while creating the topic
+                    alert("Error occurred while creating the post.");
+                }
+            },
+            error: function() {
+                // AJAX request failed
+                alert("Error occurred while making the AJAX request.");
             }
-          },
-          error: function() {
-            // AJAX request failed
-            alert("Error occurred while making the AJAX request.");
-          }
         });
     }
 
@@ -225,10 +137,9 @@ $(function() {
             data: { sort: sortCriteria, topicId: topicId },
             success: function(data) {
                 $("#postContainer").html(data);
-                shortenUpvotes();
-    
                 // Call the initializeReplyButtons function after loading the AJAX response
                 initializeReplyButtons();
+                attachDeleteButtonListeners();
             },
             error: function(xhr, status, error) {
                 console.log(xhr.responseText);
@@ -240,24 +151,103 @@ $(function() {
 
     $("#top").click(function() {
         getTopics("top"); // Pass "top" as the sort criteria
-      });
-      
-      $("#new").click(function() {
-          getTopics("new"); // Pass "new" as the sort criteria
-      });
-
-    $("#forumDeletePost").click(function(){
-        $('#confirmDeleteModalPost').modal();
-        $('#confirmDeleteModalPost').show();
     });
-
-    $("#forumDeleteComment").click(function(){
-        alert("test")
-        // $('#confirmDeleteModalComment').modal();
-        // $('#confirmDeleteModalComment').show();
+      
+    $("#new").click(function() {
+        getTopics("new"); // Pass "new" as the sort criteria
     });
 
     $(document).on('click', '#forumDeleteComment', function() {
         $('#confirmDeleteModalComment').modal('show');
     });
+
+    //delete
+
+    function attachDeleteButtonListeners() {
+        $(document).off('click', '.forumPostViewMainDelete').on('click', '.forumPostViewMainDelete', function() {
+            const topicId = $(this).attr('value');
+            $('#confirmDeleteModal').modal('show');
+            $('#confirmDeleteModal').find('#confirmDelete').off('click').on('click', function() {
+                deleteTopic(topicId);
+            });
+        });
+
+        // Delete post
+        $(document).off('click', '.forumPostViewMainDeletePost').on('click', '.forumPostViewMainDeletePost', function() {
+            const postId = $(this).attr('value');
+            $('#confirmDeleteModal').modal('show');
+            $('#confirmDeleteModal').find('#confirmDelete').off('click').on('click', function() {
+                deletePost(postId);
+            });
+        });
+    
+        // Delete reply
+        $(document).off('click', '.forumPostViewMainDeleteReply').on('click', '.forumPostViewMainDeleteReply', function() {
+            const replyId = $(this).attr('value');
+            $('#confirmDeleteModal').modal('show');
+            $('#confirmDeleteModal').find('#confirmDelete').off('click').on('click', function() {
+                deleteReply(replyId);
+            });
+        });
+    }
+    
+    // Delete a post
+    function deletePost(postId) {
+        $.ajax({
+            url: "../../ajax/discussionDelete.ajax.php",
+            method: "POST",
+            data: { postId: postId },
+            success: function(response) {
+                if (response === "success") {
+                    getPosts("", $("#topicId").val());
+                    $('#confirmDeleteModal').modal('hide');
+                } else {
+                    alert("Error occurred while deleting the post.");
+                }
+            },
+            error: function() {
+                alert("Error occurred while making the AJAX request.");
+            }
+        });
+    }
+    
+    // Delete a reply
+    function deleteReply(replyId) {
+        $.ajax({
+            url: "../../ajax/discussionDelete.ajax.php",
+            method: "POST",
+            data: { replyId: replyId },
+            success: function(response) {
+                if (response === "success") {
+                    getPosts("", $("#topicId").val());
+                    $('#confirmDeleteModal').modal('hide');
+                } else {
+                    alert("Error occurred while deleting the reply.");
+                }
+            },
+            error: function() {
+                alert("Error occurred while making the AJAX request.");
+            }
+        });
+    }
+
+    function deleteTopic(topicId) {
+        $.ajax({
+            url: "../../ajax/discussionDelete.ajax.php",
+            method: "POST",
+            data: { topicId: topicId },
+            success: function(response) {
+                if (response === "success") {
+                    window.location.href = 'discussionForum.php';
+                    $('#confirmDeleteModal').modal('hide');
+                } else {
+                    alert("Error occurred while deleting the reply.");
+                }
+            },
+            error: function() {
+                alert("Error occurred while making the AJAX request.");
+            }
+        });
+    }
+    
 });
