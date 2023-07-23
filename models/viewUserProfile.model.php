0dc3d800858e84725e2e26b9e9800925426f9f9f
JcAngeles1024 <jc.angelesmails@gmail.com>
2023-07-24 02:43:47 +0800

    update achievements uwu

--- models/viewUserProfile.model.php
+++ models/viewUserProfile.model.php
@@ -4,10 +4,11 @@ require_once "connection.php"; // Assuming connection.php contains the database 
 function getUserProfileInfo($accountid)
 {
     $connection = new Connection();
-    $db = $connection->connect();
+    $pdo = $connection->connect();
 
     try {
-        $stmt = $db->prepare("SELECT 
+        // Fetch user data from the accounts table
+        $stmt = $pdo->prepare("SELECT 
                                 (SELECT COALESCE(SUM(upvotes), 0) FROM topics WHERE accountid = :accountid) AS topicUpvotes,
                                 (SELECT COALESCE(SUM(upvotes), 0) FROM reply WHERE accountid = :accountid) AS replyUpvotes,
                                 (SELECT COALESCE(SUM(upvotes), 0) FROM posts WHERE accountid = :accountid) AS postUpvotes,
@@ -18,6 +19,39 @@ function getUserProfileInfo($accountid)
         $stmt->bindValue(':accountid', $accountid);
         $stmt->execute();
         $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // Get the number of bookmarks for the user
+        $stmt = $pdo->prepare("SELECT COUNT(*) AS bookmarks_count FROM bookmarks WHERE accountid = :accountid");
+        $stmt->bindParam(":accountid", $accountid, PDO::PARAM_STR);
+        $stmt->execute();
+        $bookmarksCount = $stmt->fetch(PDO::FETCH_ASSOC)['bookmarks_count'];
+
+        // Get the number of community creations for the user
+        $stmt = $pdo->prepare("SELECT COUNT(*) AS creations_count FROM communitycreations WHERE accountid = :accountid");
+        $stmt->bindParam(":accountid", $accountid, PDO::PARAM_STR);
+        $stmt->execute();
+        $creationsCount = $stmt->fetch(PDO::FETCH_ASSOC)['creations_count'];
+
+        // Get the number of posts for the user
+        $stmt = $pdo->prepare("SELECT COUNT(*) AS posts_count FROM posts WHERE accountid = :accountid");
+        $stmt->bindParam(":accountid", $accountid, PDO::PARAM_STR);
+        $stmt->execute();
+        $postsCount = $stmt->fetch(PDO::FETCH_ASSOC)['posts_count'];
+
+        // Get the accountDate for the user
+        $stmt = $pdo->prepare("SELECT accountDate FROM accounts WHERE accountid = :accountid");
+        $stmt->bindParam(":accountid", $accountid, PDO::PARAM_STR);
+        $stmt->execute();
+        $accountDate = $stmt->fetch(PDO::FETCH_ASSOC)['accountDate'];
+
+        // Function to check if accountDate is less than one year from the current date
+        function isAccountDateLessThanOneYear($accountDate) {
+            $accountDateTime = strtotime($accountDate);
+            $oneYearAgo = strtotime('-1 year');
+
+            return $accountDateTime > $oneYearAgo;
+        }
+        $accountDateBadge = isAccountDateLessThanOneYear($accountDate) ? 'style="filter: grayscale(1);"' : '';
 
         $topicUpvotes = $userData['topicUpvotes'] ?? 0;
         $replyUpvotes = $userData['replyUpvotes'] ?? 0;
@@ -38,10 +72,9 @@ function getUserProfileInfo($accountid)
         $nickname = $userData['nickname'];
         $username = $userData['username'];
         $religion = $userData['religion'];
-        $accountDate = $userData['accountDate'];
 
-        // Return an array containing user information
-        return array(
+        // Put all data into an array
+        $userProfileInfo = array(
             'explorerPoints' => $explorerPoints,
             'displayNotifications' => $displayNotifications,
             'displayCalendar' => $displayCalendar,
@@ -53,8 +86,32 @@ function getUserProfileInfo($accountid)
             'nickname' => $nickname,
             'username' => $username,
             'religion' => $religion,
-            'accountDate' => $accountDate
+            'accountDate' => $accountDate,
+            'bookmarkBadge10' => $bookmarksCount < 10 ? 'style="filter: grayscale(1);"' : '',
+            'bookmarkBadge50' => $bookmarksCount < 50 ? 'style="filter: grayscale(1);"' : '',
+            'bookmarkBadge100' => $bookmarksCount < 100 ? 'style="filter: grayscale(1);"' : '',
+            'bookmarkBadge250' => $bookmarksCount < 250 ? 'style="filter: grayscale(1);"' : '',
+            'bookmarkBadge500' => $bookmarksCount < 500 ? 'style="filter: grayscale(1);"' : '',
+            'postBadge10' => $postsCount < 10 ? 'style="filter: grayscale(1);"' : '',
+            'postBadge50' => $postsCount < 50 ? 'style="filter: grayscale(1);"' : '',
+            'postBadge100' => $postsCount < 100 ? 'style="filter: grayscale(1);"' : '',
+            'postBadge250' => $postsCount < 250 ? 'style="filter: grayscale(1);"' : '',
+            'postBadge500' => $postsCount < 500 ? 'style="filter: grayscale(1);"' : '',
+            'creationBadge5' => $creationsCount < 5 ? 'style="filter: grayscale(1);"' : '',
+            'creationBadge25' => $creationsCount < 25 ? 'style="filter: grayscale(1);"' : '',
+            'creationBadge50' => $creationsCount < 50 ? 'style="filter: grayscale(1);"' : '',
+            'creationBadge75' => $creationsCount < 75 ? 'style="filter: grayscale(1);"' : '',
+            'creationBadge100' => $creationsCount < 100 ? 'style="filter: grayscale(1);"' : '',
+            'explorerBadge250' => $explorerPoints < 250 ? 'style="filter: grayscale(1);"' : '',
+            'explorerBadge500' => $explorerPoints < 500 ? 'style="filter: grayscale(1);"' : '',
+            'explorerBadge1000' => $explorerPoints < 1000 ? 'style="filter: grayscale(1);"' : '',
+            'explorerBadge2500' => $explorerPoints < 2500 ? 'style="filter: grayscale(1);"' : '',
+            'explorerBadge5000' => $explorerPoints < 5000 ? 'style="filter: grayscale(1);"' : '',
+            'accountDateBadge' => $accountDateBadge
         );
+
+        // Return the user profile information array
+        return $userProfileInfo;
     } catch (PDOException $e) {
         die("Error occurred while fetching user data: " . $e->getMessage());
     }
