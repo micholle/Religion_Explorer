bbc6cd572c513e5896978a948ae0fd25694ac517
JcAngeles1024 <jc.angelesmails@gmail.com>
2023-09-04 14:50:02 +0800

    update votes tables

--- sql/post_votes.sql
+++ sql/post_votes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 18, 2023 at 08:18 AM
+-- Generation Time: Sep 04, 2023 at 08:49 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.0.28
 
@@ -31,8 +31,16 @@ CREATE TABLE `post_votes` (
   `voteId` int(10) NOT NULL,
   `postId` int(8) NOT NULL,
   `accountid` varchar(5) NOT NULL,
-  `voteType` varchar(10) NOT NULL
+  `voteType` varchar(10) NOT NULL,
+  `postVoteDate` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `post_votes`
+--
+
+INSERT INTO `post_votes` (`voteId`, `postId`, `accountid`, `voteType`, `postVoteDate`) VALUES
+(156, 12345736, 'R0001', 'upvote', '2023-09-04 14:26:09');
 
 --
 -- Indexes for dumped tables
@@ -42,7 +50,8 @@ CREATE TABLE `post_votes` (
 -- Indexes for table `post_votes`
 --
 ALTER TABLE `post_votes`
-  ADD PRIMARY KEY (`voteId`);
+  ADD PRIMARY KEY (`voteId`),
+  ADD KEY `fk_post_votes_postId` (`postId`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -52,17 +61,7 @@ ALTER TABLE `post_votes`
 -- AUTO_INCREMENT for table `post_votes`
 --
 ALTER TABLE `post_votes`
-  MODIFY `voteId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `post_votes`
---
-ALTER TABLE `post_votes`
-  ADD CONSTRAINT `fk_post_votes_postId` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`) ON DELETE CASCADE ON UPDATE CASCADE;
+  MODIFY `voteId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=157;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
